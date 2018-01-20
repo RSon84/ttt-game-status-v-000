458f4e4b64f6694640a0ec7e7d1d71937c70c5cf
Devin Wilson <craigvin84@yahoo.com>
2018-01-20 20:27:03 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,4 +3,34 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
-# Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [2,5,8],
+  [6,4,2],
+  [0,3,6],
+  [1,4,7],
+  [0,4,8]
+]
+
+def won?(board)
+winner = []
+empty_board = board.all? {|location| location == " "}
+WIN_COMBINATIONS.each do |sub_array|
+    if empty_board || full?(board)
+      return false
+    elsif sub_array.all? { |value| board[value] =="X" } || sub_array.all? { |value| board[value] =="O" }
+      winner = sub_array
+    end
+  end
+  winner
+end
+def full?(board)
+  !board.any? { |location| location == " " }
+end
+
+def draw?(board)
+  if !won?(board) && full?(board)
+  end
+end
